173ded9a9053b095e264edef757d8f181a1f7727
troy <hunter007nz@gmail.com>
2021-07-07 15:41:14 +1200

    added modal

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,34 @@
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css'
+
+const Backdrop = (props) => {
+    return (
+        <div className={classes.backdrop}>
+
+        </div>
+    )
+}
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+            <div className={classes.content}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+const portalEl = document.getElementById('overlays')
+
+const Modal = (props) => {
+    return (
+        <>
+            {ReactDOM.createPortal(<Backdrop />, portalEl)}
+            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalEl)}
+        </>
+    )
+}
+
+
+export default Modal
